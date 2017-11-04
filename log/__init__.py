28a1a04790b460e3f657ceaaaab8415b23d4c23c
Ron Pandolfi <ronpandolfi@gmail.com>
2017-11-03 23:31:07 -0700

    Add blacklist filter.

--- log/__init__.py
+++ log/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 from qtpy.QtCore import *
 from qtpy.QtGui import *
@@ -11,21 +13,30 @@ colors = {msg.DEBUG: Qt.gray, msg.ERROR: Qt.darkRed, msg.CRITICAL: Qt.red,
 from xicam.plugins import GUIPlugin, GUILayout
 
 
-class LogPlugin(GUIPlugin):
+class BlacklistFilter(logging.Filter):
+    blacklist = ['ipykernel.inprocess.ipkernel', 'traitlets']
+
+    def filter(self, record):
+        return record.name not in self.blacklist
+
+
+class LogPlugin(GUIPlugin, logging.Handler):
     name = 'Log'
     sigLog = Signal(int, str, str, np.ndarray)
 
     def __init__(self, *args, **kwargs):
         self.logwidget = QListWidget()
         self.stages = {'Log': GUILayout(self.logwidget)}
-
-        # msg.guilogcallable = self.log
-
         super(LogPlugin, self).__init__(*args, **kwargs)
+        logging.Handler.__init__(self)
+        self.level = msg.DEBUG
+        self.filters = [BlacklistFilter()]
 
-    def log(self, level, timestamp, s, image=None, icon=None):  # We can have icons!
-        item = QListWidgetItem(s)
-        item.setForeground(QBrush(colors[level]))
+        logging.getLogger().addHandler(self)
+
+    def emit(self, record, level=msg.INFO, timestamp=None, image=None, icon=None):  # We can have icons!
+        item = QListWidgetItem(record.msg)
+        item.setForeground(QBrush(colors[record.levelno]))
         item.setToolTip(timestamp)
         self.logwidget.addItem(item)
         if image is not None:
